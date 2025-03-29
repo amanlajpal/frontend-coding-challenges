c03171233648cb6e3b6e6e208b21be8bdbe8ab62
Aman Lajpal <42869088+amanlajpal@users.noreply.github.com>
2025-03-29 20:08:17 +0530

    Refactor counter buttons in Home component for improved functionality and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,31 @@ import { useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
+  function handleIncCount() {
+    setCount((prevCount) => {
+      return prevCount + 1;
+    });
+  }
+  function handleResetCount() {
+    setCount(0);
+  }
   return (
     <div>
       <div>Counter: {count}</div>
-      <button
-        onClick={() => {
-          setCount((prevCount) => {
-            return prevCount + 1;
-          });
-        }}
-      >Inc</button>
+      <div className="flex gap-2">
+        <button
+          className="bg-cyan-500 px-4 py-2 rounded-lg"
+          onClick={handleIncCount}
+        >
+          +
+        </button>
+        <button
+          className="bg-red-500 px-4 py-2 rounded-lg"
+          onClick={handleResetCount}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
